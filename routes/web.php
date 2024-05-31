834605e0db36cbab90c28f89a298f036d331614b
sloveniangooner <lovro.papez@gmail.com>
2024-05-31 12:38:25 +0200

    Try something.

--- routes/web.php
+++ routes/web.php
@@ -5,26 +5,29 @@ use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Schema;
 
-if (Schema::hasTable('pages')) {
-    Route::get('/', function () {
-        return view('welcome');
-    })->name('home');
+try {
+    if (Schema::hasTable('pages')) {
+        Route::get('/', function () {
+            return view('welcome');
+        })->name('home');
 
-    foreach (Page::orderBy('_lft')->get() as $page) {
-        Route::get($page->getNestedSlug(), function () use ($page) {
-            return view('page', compact('page'));
-        })->name($page->getRouteName());
+        foreach (Page::orderBy('_lft')->get() as $page) {
+            Route::get($page->getNestedSlug(), function () use ($page) {
+                return view('page', compact('page'));
+            })->name($page->getRouteName());
+        }
+
+        foreach (Post::query()->get() as $post) {
+            Route::get(config('filament.posts_slug') . '/' . $post->slug, function () use ($post) {
+                return view('post', compact('post'));
+            })->name($post->getRouteName());
+        }
+
+        Route::get(config('filament.posts_slug'), function () {
+            return view('posts', [
+                'posts' => Post::query()->latest()->paginate(),
+            ]);
+        })->name('posts');
     }
-
-    foreach (Post::query()->get() as $post) {
-        Route::get(config('filament.posts_slug') . '/' . $post->slug, function () use ($post) {
-            return view('post', compact('post'));
-        })->name($post->getRouteName());
-    }
-
-    Route::get(config('filament.posts_slug'), function () {
-        return view('posts', [
-            'posts' => Post::query()->latest()->paginate(),
-        ]);
-    })->name('posts');
+} catch (Exception $e) {
 }
